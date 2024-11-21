9434959fd1bf159e44350c4444a2cb4e23c52f82
Alm3g <almoghash13@gmail.com>
2024-11-21 09:52:26 +0200

    Added UserClass

--- app/src/main/java/com/example/mybarber/SignUp.java
+++ app/src/main/java/com/example/mybarber/SignUp.java
@@ -1,6 +1,12 @@
 package com.example.mybarber;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,58 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
+EditText displayName,Email,Password,ConfirmPassword;
+RadioGroup UserTypeGroup;
+RadioButton BarberButton,CustomerButton;
+Boolean IsBarber;
+Button SignUp;
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (BarberButton.isChecked())
+            IsBarber=true;
+        if (CustomerButton.isChecked())
+            IsBarber=false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+       displayName=findViewById(R.id.displayname);
+       Email=findViewById(R.id.email);
+       Password=findViewById(R.id.password);
+       ConfirmPassword=findViewById(R.id.confirmpassword);
+       UserTypeGroup=findViewById(R.id.userTypeGroup);
+       BarberButton=findViewById(R.id.barberButton);
+       CustomerButton=findViewById(R.id.customerButton);
+       BarberButton.setOnCheckedChangeListener(this);
+       CustomerButton.setOnCheckedChangeListener(this);
+       SignUp=findViewById(R.id.signupbtn);
+       SignUp.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view==SignUp){
+            String username=displayName.getText().toString();
+            String email=Email.getText().toString();
+            String password=Password.getText().toString();
+
+
+
+
+            User user=new User(username,email,password,IsBarber);
+        }
     }
 }
+
+
+
+
+
---
+++ app/src/main/java/com/example/mybarber/User.java
@@ -0,0 +1,58 @@
+package com.example.mybarber;
+
+public class User {
+
+        private String DisplayName;
+        private String Email;
+        private String Password;
+        private Boolean IsBarber;
+
+    public User(String displayName, String email, String password, Boolean isBarber) {
+        DisplayName = displayName;
+        Email = email;
+        Password = password;
+        IsBarber = isBarber;
+    }
+
+    public String getDisplayName() {
+        return DisplayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        DisplayName = displayName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public Boolean getBarber() {
+        return IsBarber;
+    }
+
+    public void setBarber(Boolean barber) {
+        IsBarber = barber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "DisplayName='" + DisplayName + '\'' +
+                ", Email='" + Email + '\'' +
+                ", Password='" + Password + '\'' +
+                ", IsBarber=" + IsBarber +
+                '}';
+    }
+}
